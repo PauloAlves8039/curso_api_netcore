80443759d19f90707effd6ce015d9f12aadf22ed
PauloAlves8039 <pj38alves@gmail.com>
2021-01-29 13:54:20 -0300

    Implementando o UserService

---
+++ src/Api.Service/Services/UserService.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using src.Api.Domain.Entities;
+using src.Api.Domain.Interfaces;
+using src.Api.Domain.Interfaces.Services.User;
+
+namespace src.Api.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepository<UserEntity> _repository;
+
+        public UserService(IRepository<UserEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _repository.SelectAsync();
+        }
+        public async Task<UserEntity> Post(UserEntity user)
+        {
+            return await _repository.InsertAsync(user);
+        }
+
+        public async Task<UserEntity> Put(UserEntity user)
+        {
+            return await _repository.UpdateAsync(user);
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
