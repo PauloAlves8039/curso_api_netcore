476deaa7c284fe1ad4caf122328f024a703f03b9
PauloAlves8039 <pj38alves@gmail.com>
2021-02-07 10:47:50 -0300

    Adicionando injeção de dependência no CrossCutting

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using src.Api.Data.Context;
+using src.Api.Data.Implementations;
 using src.Api.Data.Repository;
 using src.Api.Domain.Interfaces;
+using src.Api.Domain.Repository;
 
 namespace src.Api.CrossCutting.DependencyInjection
 {
@@ -11,6 +13,7 @@ namespace src.Api.CrossCutting.DependencyInjection
         public static void ConfigureDependencyRepository(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
 
             serviceCollection.AddDbContext<MyContext>
             (options => options.UseMySql("Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=******"));
