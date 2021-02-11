fef5c30bf6fea701382083ea5275da93ee6b898d
PauloAlves8039 <pj38alves@gmail.com>
2021-02-11 09:35:33 -0300

    Criando DTOs para User

---
+++ src/Api.Domain/Dtos/User/UserDtoUpdateResult.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace src.Api.Domain.Dtos.User
+{
+    public class UserDtoUpdateResult
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
