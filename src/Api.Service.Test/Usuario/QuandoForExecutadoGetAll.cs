38e4890fa35d182e07aba2d17bf5f0bf045ce79d
PauloAlves8039 <pj38alves@gmail.com>
2021-02-22 20:09:30 -0300

    Adicionando a classe QuandoForExecutadoGetAll

---
+++ src/Api.Service.Test/Usuario/QuandoForExecutadoGetAll.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using src.Api.Domain.Dtos.User;
+using src.Api.Domain.Interfaces.Services.User;
+using Xunit;
+
+namespace src.Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoGetAll : UsuarioTestes
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É possivel executar o método GETALL.")]
+        public async Task E_Possivel_Executar_Metodo_GetAll()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(listaUserDto);
+            _service = _serviceMock.Object;
+
+            var result = await _service.GetAll();
+            Assert.NotNull(result);
+            Assert.True(result.Count() == 10);
+
+
+            var _listResult = new List<UserDto>();
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.GetAll()).ReturnsAsync(_listResult.AsEnumerable);
+            _service = _serviceMock.Object;
+
+            var _resultEmpty = await _service.GetAll();
+            Assert.Empty(_resultEmpty);
+            Assert.True(_resultEmpty.Count() == 0);
+
+        }
+    }
+}
