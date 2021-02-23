a4f32f97545bcbe6fa244c2ab6360ff057bf6997
PauloAlves8039 <pj38alves@gmail.com>
2021-02-23 09:43:25 -0300

    Adicionando a classe QuandoForExecutadoCreate

---
+++ src/Api.Service.Test/Usuario/QuandoForExecutadoCreate.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Moq;
+using src.Api.Domain.Interfaces.Services.User;
+using Xunit;
+
+namespace src.Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoCreate : UsuarioTestes
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É possivel executar o método POST.")]
+        public async Task E_Possivel_Executar_Metodo_Post()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Post(userDtoCreate)).ReturnsAsync(userDtoCreateResult);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Post(userDtoCreate);
+            Assert.NotNull(result);
+            Assert.Equal(NomeUsuario, result.Name);
+            Assert.Equal(EmailUsuario, result.Email);
+        }
+    }
+}
