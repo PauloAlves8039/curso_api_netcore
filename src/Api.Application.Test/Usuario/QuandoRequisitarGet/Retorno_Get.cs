ebcbc773538e5e2b6d188d146fb57c3b73a22faa
PauloAlves8039 <pj38alves@gmail.com>
2021-03-01 12:05:02 -0300

    Criando testes para UsersController-GET

---
+++ src/Api.Application.Test/Usuario/QuandoRequisitarGet/Retorno_Get.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using src.Api.Application.Controllers;
+using src.Api.Domain.Dtos.User;
+using src.Api.Domain.Interfaces.Services.User;
+using Xunit;
+
+namespace src.Api.Application.Test.Usuario.QuandoRequisitarGet
+{
+    public class Retorno_Get
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É possível Realizar o Get.")]
+        public async Task E_Possivel_Invocar_a_Controller_Get()
+        {
+            var serviceMok = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMok.Setup(m => m.Get(It.IsAny<Guid>())).ReturnsAsync(
+                new UserDto
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMok.Object);
+            var result = await _controller.Get(Guid.NewGuid());
+            Assert.True(result is OkObjectResult);
+            var resultValue = ((OkObjectResult)result).Value as UserDto;
+            Assert.NotNull(resultValue);
+            Assert.Equal(nome, resultValue.Name);
+            Assert.Equal(email, resultValue.Email);
+        }
+    }
+}
