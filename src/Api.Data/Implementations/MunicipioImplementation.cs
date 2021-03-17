64cbf7256b0d134cd8bfbfb90403315655c039fd
PauloAlves8039 <pj38alves@gmail.com>
2021-03-16 21:04:33 -0300

    Implementando testes unitários para Municipio

--- src/Api.Data/Implementations/MunicipioImplementation.cs
+++ src/Api.Data/Implementations/MunicipioImplementation.cs
@@ -24,7 +24,7 @@ namespace src.Api.Data.Implementations
 
         public async Task<MunicipioEntity> GetCompleteById(Guid id)
         {
-            return await _dataset.Include(m => m.Uf).FirstOrDefaultAsync(m => m.CodIBGE.Equals(id));
+            return await _dataset.Include(m => m.Uf).FirstOrDefaultAsync(m => m.Id.Equals(id));
         }
     }
 }
