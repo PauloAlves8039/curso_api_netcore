0ed267ec564da5270418bffc2b972a26276eb96c
PauloAlves8039 <pj38alves@gmail.com>
2021-03-20 19:36:31 -0300

    Implementando Service Testes de Município

---
+++ src/Api.Service.Test/Municipio/QuandoForExecutadoDelete.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Moq;
+using src.Api.Domain.Interfaces.Services.Municipio;
+using Xunit;
+
+namespace src.Api.Service.Test.Municipio
+{
+    public class QuandoForExecutadoDelete : MunicipioTestes
+    {
+        private IMunicipioService _service;
+        private Mock<IMunicipioService> _serviceMock;
+
+        [Fact(DisplayName = "É possível executar método Delete.")]
+        public async Task E_Possivel_Executar_Metodo_Delete()
+        {
+            _serviceMock = new Mock<IMunicipioService>();
+            _serviceMock.Setup(m => m.Delete(IdMunicipio))
+                        .ReturnsAsync(true);
+            _service = _serviceMock.Object;
+
+            var deletado = await _service.Delete(IdMunicipio);
+            Assert.True(deletado);
+
+            _serviceMock = new Mock<IMunicipioService>();
+            _serviceMock.Setup(m => m.Delete(It.IsAny<Guid>()))
+                        .ReturnsAsync(false);
+            _service = _serviceMock.Object;
+
+            deletado = await _service.Delete(Guid.NewGuid());
+            Assert.False(deletado);
+        }
+    }
+}
---
+++ src/Api.Service.Test/Municipio/QuandoForExecutadoUpdate.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Moq;
+using src.Api.Domain.Interfaces.Services.Municipio;
+using Xunit;
+
+namespace src.Api.Service.Test.Municipio
+{
+    public class QuandoForExecutadoUpdate : MunicipioTestes
+    {
+        private IMunicipioService _service;
+        private Mock<IMunicipioService> _serviceMock;
+
+        [Fact(DisplayName = "É Possivel executar o Método Update.")]
+        public async Task E_Possivel_Executar_Metodo_Update()
+        {
+            _serviceMock = new Mock<IMunicipioService>();
+            _serviceMock.Setup(m => m.Put(municipioDtoUpdate)).ReturnsAsync(municipioDtoUpdateResult);
+            _service = _serviceMock.Object;
+
+            var resultUpdate = await _service.Put(municipioDtoUpdate);
+            Assert.NotNull(resultUpdate);
+            Assert.Equal(NomeMunicipioAlterado, resultUpdate.Nome);
+            Assert.Equal(CodigoIBGEMunicipioAlterado, resultUpdate.CodIBGE);
+            Assert.Equal(IdUf, resultUpdate.UfId);
+        }
+    }
+}
