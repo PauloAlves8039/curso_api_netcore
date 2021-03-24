6a702dadb4490a1d0ead4eaa1880d67485050bce
PauloAlves8039 <pj38alves@gmail.com>
2021-03-23 22:39:56 -0300

    Definição de testes de aplicação para UF GetAll

---
+++ src/Api.Application.Test/Uf/QuandoRequisitarGetAll/Retorno_OK.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using src.Api.Application.Controllers;
+using src.Api.Domain.Dtos.Uf;
+using src.Api.Domain.Interfaces.Services.Uf;
+using Xunit;
+
+namespace src.Api.Application.Test.Uf.QuandoRequisitarGetAll
+{
+    public class Retorno_OK
+    {
+        private UfsController _controller;
+
+        [Fact(DisplayName = "É possível Realizar o GetAll.")]
+        public async Task E_Possivel_Invocar_a_Controller_GetAll()
+        {
+            var serviceMock = new Mock<IUfService>();
+            serviceMock.Setup(m => m.GetAll()).ReturnsAsync(
+                 new List<UfDto>
+                 {
+                    new UfDto
+                    {
+                        Id = Guid.NewGuid(),
+                        Nome = "São Paulo",
+                        Sigla = "SP",
+                    },
+                    new UfDto
+                    {
+                        Id = Guid.NewGuid(),
+                        Nome = "Amazonas",
+                        Sigla = "AM",
+                    }
+                 }
+            );
+
+            _controller = new UfsController(serviceMock.Object);
+            var result = await _controller.GetAll();
+            Assert.True(result is OkObjectResult);
+
+        }
+    }
+}
